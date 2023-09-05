5f6934548f0be2158d446d96363da219bf293ec1
Emma8826 <j852641@gmail.com>
2023-09-05 22:54:51 +0800

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+const app =  Vue.createApp({
+    //data, functions
+    data(){
+        //return data object
+        return{
+            showMovie : true,
+            title:'The final empire',
+            ep:'Session 1',
+            time:123
+        }
+    },
+    methods:{
+        change(title,time){
+            console.log('Click button')
+            this.title =  title
+            this.time = time
+        },
+        toggleShowBook(){
+            this.showMovie = !this.showMovie
+        }
+    }
+})
+//mount = 發動
+app.mount('#app')
