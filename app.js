7dc202f69b7565d3be16be6b0992fd0cc6c34b5a
Emma8826 <j852641@gmail.com>
2023-09-12 00:35:09 +0800

    image practice

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ const app =  Vue.createApp({
         return{
             url: 'https://www.notion.so/Jenny-Lin-Note-d45c5182333943c0b7616a1972e31fb1',
             showMovie : true,
+            showBooks:true,
             title:'The final empire',
             ep:'Session 1',
             time:123,
-            showBooks:true,
             books:[
-                {title:'An apple',author:'A'},
-                {title:'Banana',author:'B'},
-                {title:'Cat',author:'C'}
+                {title:'An apple',picName:'A',img:'../sql.png'},
+                {title:'Banana',picName:'B',img:'/../sqlserver.png'}
             ],
             x:0,y:0,          
         }
@@ -35,6 +34,11 @@ const app =  Vue.createApp({
         handleMousemove(e){
             this.x = e.offsetX
             this.Y = e.offsetY
+        }
+    },
+    filters:{//filters的屬性，裡面是放你想要怎麼修改資料的函數
+        toUpper:function(obj){
+            return obj.toUpperCase();
         }
     }
 })
