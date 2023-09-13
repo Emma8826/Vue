1eada4f1d8d8510b388085d3c57bcdc368d485bb
Emma8826 <j852641@gmail.com>
2023-09-13 11:40:31 +0800

    Book Shop Page

--- app.js
+++ app.js
@@ -3,42 +3,35 @@ const app =  Vue.createApp({
     data(){
         //return data object
         return{
-            url: 'https://www.notion.so/Jenny-Lin-Note-d45c5182333943c0b7616a1972e31fb1',
-            showMovie : true,
-            showBooks:true,
-            title:'The final empire',
-            ep:'Session 1',
-            time:123,
-            books:[
-                {title:'An apple',picName:'A',img:'../sql.png'},
-                {title:'Banana',picName:'B',img:'/../sqlserver.png'}
-            ],
-            x:0,y:0,          
+            url: 'https://www.notion.so/Jenny-Lin-Note-d45c5182333943c0b7616a1972e31fb1',      
+            showBooks:true,           
+            Books:[
+                {title:'底層邏輯',author:'劉潤',img:'../20220318054356524735.jpg',price:330,isFav:true},
+                {title:'理科工作術',author:'井下田久幸',price:300,img:'/../415_112937271_236_mainCoverImage1.jpg',isFav:false},
+                {title:'原子習慣',author:'詹姆斯·克利爾',price:320,img:'/../ec1760858.jpg',isFav:true}
+            ]        
         }
     },
     methods:{
-        change(title,time){
-            console.log('Click button')
-            this.title =  title
-            this.time = time
-        },
         toggleShowBook(){
-            this.showMovie = !this.showMovie
+            this.showBooks = !this.showBooks
         },
-        handleEvent(e,data){
-            console.log(e,e.type)
-            if(data != null){
-                console.log(data)
-            }
+        togglefav(book){
+            book.isFav = !book.isFav
         },
-        handleMousemove(e){
-            this.x = e.offsetX
-            this.Y = e.offsetY
-        }
+        toggleShowDetails(){
+
+        }        
     },
-    filters:{//filters的屬性，裡面是放你想要怎麼修改資料的函數
-        toUpper:function(obj){
-            return obj.toUpperCase();
+    computed: { // 存放每一个計算函數
+        favTotal() {
+            return this.Books.filter((book) => book.isFav).length 
+        },
+        priceTotal() {
+            return this.Books
+            .filter((book) => book.isFav)           
+            .reduce((total, book) => total + book.price, 0);//(accumulator,currentItem)
+            //reduce 會按順序遍歷數組中的每個元素(book)，將book.price加總到total中
         }
     }
 })
